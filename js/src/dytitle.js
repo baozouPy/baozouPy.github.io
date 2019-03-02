b8bc58443916cb342323781bebcfb1a4fcb888c8
暴走 <baozou@baozoudeMBP.lan>
2019-03-02 11:44:13 +0800

    Site updated: 2019-03-02 11:44:13

--- js/src/dytitle.js
+++ js/src/dytitle.js
@@ -4,12 +4,12 @@ var titleTime;
 document.addEventListener('visibilitychange', function () {
     if (document.hidden) {
         $('[rel="icon"]').attr('href', "/img/TEP.ico");
-        document.title = ' 你不理我了！';
+        document.title = ' 辣鸡博客崩溃啦！(ÒωÓױ)呃！！！！';
         clearTimeout(titleTime);
     }
     else {
         $('[rel="icon"]').attr('href', "/favicon.ico");
-        document.title = ' 么么哒 ' + OriginTitile;
+        document.title = ' 又好了 ( /) V (\ ) 嘻嘻~' + OriginTitile;
         titleTime = setTimeout(function () {
             document.title = OriginTitile;
         }, 2000);
